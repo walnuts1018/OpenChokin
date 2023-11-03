e1bbc86f4022c418395350f03ab4248c7a68c872
walnuts1018 <r.juglans.1018@gmail.com>
2023-11-03 15:51:52 +0900

    add

--- back/domain/db.go
+++ back/domain/db.go
@@ -9,9 +9,20 @@ type DB interface {
 	GetMoneyPool(id string) (MoneyPool, error)
 	GetMoneyPoolsByUserID(userID string) ([]MoneyPool, error)
 	UpdateMoneyPool(moneyPool MoneyPool) error
+	UpdateMoneyPoolShareUserIDs(moneyPoolID string, shareUserIDs []string) error
 
 	NewMoneyProvider(moneyProvider MoneyProvider) error
 	GetMoneyProvider(id string) (MoneyProvider, error)
 	GetMoneyProvidersByUserID(userID string) ([]MoneyProvider, error)
 	UpdateMoneyProvider(moneyProvider MoneyProvider) error
+
+	NewStore(store Store) error
+	GetStore(id string) (Store, error)
+	GetStoresByUserID(userID string) ([]Store, error)
+	UpdateStore(store Store) error
+
+	NewItem(item Item) error
+	GetItem(id string) (Item, error)
+	GetItemsByUserID(userID string) ([]Item, error)
+	UpdateItem(item Item) error
 }
