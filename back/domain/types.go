3b7ef5b15bdc01941ad044a0b67f39151ef2690b
walnuts1018 <r.juglans.1018@gmail.com>
2023-11-03 15:53:09 +0900

    Add version field to domain models

--- back/domain/types.go
+++ back/domain/types.go
@@ -12,6 +12,7 @@ type MoneyTransaction struct {
 	IsWorldPublic   bool      `db:"is_world_public"`
 	IsExpectation   bool      `db:"is_expectation"`
 	StoreID         int64     `db:"store_id"`
+	Version         int64     `db:"version"`
 }
 
 type User struct {
@@ -19,9 +20,10 @@ type User struct {
 }
 
 type Store struct {
-	ID     int64  `db:"id"`
-	Name   string `db:"name"`
-	UserID int64  `db:"user_id"`
+	ID      int64  `db:"id"`
+	Name    string `db:"name"`
+	UserID  int64  `db:"user_id"`
+	Version int64  `db:"version"`
 }
 
 type MoneyPool struct {
@@ -31,6 +33,7 @@ type MoneyPool struct {
 	Color         string `db:"color"`
 	IsWorldPublic bool   `db:"is_world_public"`
 	OwnerID       int64  `db:"owner_id"`
+	Version       int64  `db:"version"`
 }
 
 type Item struct {
@@ -38,6 +41,7 @@ type Item struct {
 	Name         string  `db:"name"`
 	PricePerUnit float64 `db:"price_per_unit"`
 	UserID       int64   `db:"user_id"`
+	Version      int64   `db:"version"`
 }
 
 type MoneyProvider struct {
@@ -45,4 +49,5 @@ type MoneyProvider struct {
 	Name    string  `db:"name"`
 	UserID  int64   `db:"user_id"`
 	Balance float64 `db:"balance"`
+	Version int64   `db:"version"`
 }
